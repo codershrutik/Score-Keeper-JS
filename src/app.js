6480e1d24b038fd5d4d816baa5c9bf6b7a2f09c8
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-10 10:57:21 +0000

    Reset button added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const p1button = document.querySelector('#p1button');
 const p2button = document.querySelector('#p2button');
+const reset = document.
+querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+
+const winningScoreSelect = document.querySelector('#playto')
 
 let p1Score = 0;
 let p2Score = 0;
@@ -27,3 +31,18 @@ p2button.addEventListener('click',function(){
     }
     p2Display.textContent = p2Score;
 })
+
+winningScoreSelect.addEventListener('change',function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+reset.addEventListener('click',reset)
+
+function reset(){
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Score.textContent = 0;
+    p2Score.textContent = 0;
+}
